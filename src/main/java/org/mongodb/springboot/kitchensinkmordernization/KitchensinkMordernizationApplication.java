6c07e31b9bec1dca1c8ec7dba067b470e4b77a98
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-02-27 15:24:12 +0530

    Initial boilerplate

---
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/KitchensinkMordernizationApplication.java
@@ -0,0 +1,13 @@
+package org.mongodb.springboot.kitchensinkmordernization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkMordernizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchensinkMordernizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mongodb/springboot/kitchensinkmordernization/KitchensinkMordernizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mongodb.springboot.kitchensinkmordernization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchensinkMordernizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
