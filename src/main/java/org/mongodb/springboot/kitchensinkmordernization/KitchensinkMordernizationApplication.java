21ff9d8147ec03e7bbd42e6af777ee273cf53d05
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-02-28 23:17:42 +0530

    Removed Swagger Dependencies

--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/KitchensinkMordernizationApplication.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/KitchensinkMordernizationApplication.java
@@ -1,14 +1,11 @@
 package org.mongodb.springboot.kitchensinkmordernization;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
-@OpenAPIDefinition(info = @Info(title = "KitchenSink APIs",version = "1.0.0",description = "Apis for KitchenSink"))
 public class KitchensinkMordernizationApplication {
 
     public static void main(String[] args) {
