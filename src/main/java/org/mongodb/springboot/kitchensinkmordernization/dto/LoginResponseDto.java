51039ac5c3c14ba93fe0442d3b88467f11fdb073
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-03-01 17:55:33 +0530

    Added role to the response

--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/dto/LoginResponseDto.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/dto/LoginResponseDto.java
@@ -3,6 +3,9 @@ package org.mongodb.springboot.kitchensinkmordernization.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.mongodb.springboot.kitchensinkmordernization.enums.MemberRole;
+
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -10,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class LoginResponseDto {
     private String jwt;
     Long userId;
+    Set<MemberRole> roles;
 }
