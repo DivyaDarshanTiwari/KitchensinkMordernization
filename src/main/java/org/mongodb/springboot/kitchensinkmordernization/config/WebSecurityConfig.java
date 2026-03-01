5cb9e764c0cb7c6a64d7a09824732cebd7b2959b
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-03-01 17:58:34 +0530

    From Arrays.ofList() to List.of()

--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/config/WebSecurityConfig.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -80,9 +80,9 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
