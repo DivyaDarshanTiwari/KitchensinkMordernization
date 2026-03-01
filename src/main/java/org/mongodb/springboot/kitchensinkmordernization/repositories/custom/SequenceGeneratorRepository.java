5a653971b66767d823e7abaf37f994ac7c50d23b
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-03-01 22:13:06 +0530

    Now the SequenceGeneratorRepository  acts as a  mongodb repo and transaction is working

--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/repositories/custom/SequenceGeneratorRepository.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/repositories/custom/SequenceGeneratorRepository.java
@@ -1,8 +1,10 @@
 package org.mongodb.springboot.kitchensinkmordernization.repositories.custom;
 
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableMongoRepositories(basePackageClasses = SequenceGeneratorRepository.class)
 public interface SequenceGeneratorRepository {
     public Long generateSequenceByName(String name);
 }
