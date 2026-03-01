939060af299687d9255e90aa3833804db9b3dc56
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-03-01 23:46:56 +0530

    added Code for Swagger Documentation

--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/controllers/AuthController.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/controllers/AuthController.java
@@ -1,8 +1,15 @@
 package org.mongodb.springboot.kitchensinkmordernization.controllers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.mongodb.springboot.kitchensinkmordernization.dto.ApiError;
 import org.mongodb.springboot.kitchensinkmordernization.dto.LoginRequestDto;
 import org.mongodb.springboot.kitchensinkmordernization.dto.LoginResponseDto;
 import org.mongodb.springboot.kitchensinkmordernization.dto.MemberDTO;
@@ -20,11 +27,29 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Used for Login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User is Authentic",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberDTO.class))}),
+            @ApiResponse(responseCode = "401", description = "Users Not authentic",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = MemberDTO.class)))}),
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> logic(@RequestBody LoginRequestDto loginRequestDto) {
         return ResponseEntity.ok(authService.login(loginRequestDto));
     }
 
+    @Operation(summary = "Used for sigup")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "SignUp Successful",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberDTO.class))}),
+            @ApiResponse(responseCode = "409", description = "Duplicate data found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))}),
+    })
     @PostMapping("/signUp")
     public ResponseEntity<Void> createMember(@RequestBody @Valid MemberDTO memberDTO) {
         authService.signUp(memberDTO);
--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/controllers/MemberController.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/controllers/MemberController.java
@@ -1,7 +1,14 @@
 package org.mongodb.springboot.kitchensinkmordernization.controllers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.mongodb.springboot.kitchensinkmordernization.dto.ApiError;
 import org.mongodb.springboot.kitchensinkmordernization.dto.MemberDTO;
 import org.mongodb.springboot.kitchensinkmordernization.services.MemberService;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +24,41 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "Get user detail for a particular userId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the User",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "403", description = "Unauthorized",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))})
+    })
     @GetMapping("/{id}")
     public ResponseEntity<MemberDTO> getMember(@PathVariable Long id) {
         return ResponseEntity.ok(memberService.getMemberById(id));
     }
 
+    @Operation(summary = "Get details of all the users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the User",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = MemberDTO.class)))}),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "403", description = "Unauthorized",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class))})
+    })
     @GetMapping
     public List<MemberDTO> findAllMembers() {
         return memberService.findAllMembers();
