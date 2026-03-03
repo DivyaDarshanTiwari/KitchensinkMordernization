8827be0cb8438774b7b6f24b3d6a9b69b17058c2
DivyaDarshanTiwari <davidrocco400@outlook.com>
2026-03-03 13:58:16 +0530

    Null checking updated

--- src/main/java/org/mongodb/springboot/kitchensinkmordernization/services/implementation/AuthServiceImpl.java
+++ src/main/java/org/mongodb/springboot/kitchensinkmordernization/services/implementation/AuthServiceImpl.java
@@ -14,6 +14,7 @@ import org.mongodb.springboot.kitchensinkmordernization.services.AuthService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,7 +38,9 @@ public class AuthServiceImpl implements AuthService {
                 new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail().toLowerCase(), loginRequestDto.getPassword()));
         Member member = (Member) authentication.getPrincipal();
 
-        assert member != null;
+        if(member == null) {
+            throw new UsernameNotFoundException(loginRequestDto.getEmail());
+        }
         String token = authUtil.generateJwtToken(member);
         return new LoginResponseDto(token, member.getId(), member.getRole());
     }
